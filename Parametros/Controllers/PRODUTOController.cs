1694088699b951fdb4c45032460ce8a55c4a198e
alexandre santos <pcj92@hotmail.com>
2019-03-28 21:31:30 -0400

    Aula 28/03

---
+++ Parametros/Controllers/PRODUTOController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parametros.Controllers
+{
+    public class PRODUTOController : Controller
+    {
+
+        public ActionResult Buscar(int prd)
+        {
+            string retorno = string.Empty;
+            string linkretorno = string.Empty;
+            switch(prd)
+            {
+                case 1:
+                    retorno = "sansung galaxy";
+                    linkretorno = "https://www.samsung.com/br/smartphones/galaxy-s9/";
+                    break;
+
+                case 5:
+                    retorno = "iphone 3";
+                    linkretorno = "https://www.apple.com/br/iphone-xs/";
+                    break;
+
+                default:
+                    retorno = "Nokia";
+                    linkretorno = "https://br.gearbest.com/cell-phones/pp_009787976351.html";
+                    break;
+                    
+
+            }
+             
+            ViewBag.Produto = retorno;
+            ViewBag.link = linkretorno;
+            return View();
+        }
+    }
+}
---
+++ Parametros/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Parametros.MvcApplication" Language="C#" %>
